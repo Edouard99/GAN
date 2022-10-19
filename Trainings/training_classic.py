05852250474eb258171eb1fe0a9d693d7eef09fa
Edouard Castets <castetscrassoux@yahoo.fr>
2022-10-19 19:17:19 +0200

    update

--- Trainings/training_classic.py
+++ Trainings/training_classic.py
@@ -14,6 +14,11 @@ def BCEsmooth(input,target,device):
   criterion = torch.nn.BCELoss().to(device)
   return criterion(input,target_ls)
 
+def Normalization(fake):
+  for i in range(0,3):
+      fake[i]=(fake[i]-fake[i].min())/(fake[i].max()-fake[i].min())
+  return fake
+
 def train(dataloader,netD,netG,optimizerD,optimizerG,num_epochs,device,savenet,pathsavenet,pathsaveimg,fixed_noise):
     # Lists to keep track of progress
     img_list = []
