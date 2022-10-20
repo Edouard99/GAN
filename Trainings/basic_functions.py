7fac9c9b555be7c3f4345895e7cdbc3ea1759449
Edouard Castets <castetscrassoux@yahoo.fr>
2022-10-20 11:48:55 +0200

    up

--- Trainings/basic_functions.py
+++ Trainings/basic_functions.py
@@ -1,6 +1,9 @@
 import torch
 
-
+def BCEsmooth(input,target,device):
+  target_ls= target*(1-0.1)+0.1/2
+  criterion = torch.nn.BCELoss().to(device)
+  return criterion(input,target_ls)
 
 def Normalization(fake):
   for i in range(0,3):
--- Trainings/training_classic.py
+++ Trainings/training_classic.py
@@ -9,16 +9,6 @@ import numpy as np
 from IPython.display import display
 
 
-def BCEsmooth(input,target,device):
-  target_ls= target*(1-0.1)+0.1/2
-  criterion = torch.nn.BCELoss().to(device)
-  return criterion(input,target_ls)
-
-def Normalization(fake):
-  for i in range(0,3):
-      fake[i]=(fake[i]-fake[i].min())/(fake[i].max()-fake[i].min())
-  return fake
-
 def train(dataloader,netD,netG,optimizerD,optimizerG,num_epochs,device,savenet,pathsavenet,pathsaveimg,fixed_noise):
     # Lists to keep track of progress
     img_list = []
@@ -32,8 +22,6 @@ def train(dataloader,netD,netG,optimizerD,optimizerG,num_epochs,device,savenet,p
     print("Starting Training Loop...")
     for epoch in range(num_epochs):
         for i, data in enumerate(dataloader, 0):
-            netD.train()
-            netG.train()
             ############################
             # (1) Update D network: minimize BCELOSS : -( log(D(x)) + log(1 - D(G(z))) )
             ############################
@@ -43,6 +31,11 @@ def train(dataloader,netD,netG,optimizerD,optimizerG,num_epochs,device,savenet,p
             b_size = real_cpu.size(0)
             label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
             output = netD(real_cpu).view(-1)
+            if iters==0:
+              zizi=output
+              zozo=label
+              print(zizi.detach().cpu().numpy())
+              print(zozo.detach().cpu().numpy())
             errD_real = BCEsmooth(output, label,device)
             errD_real.backward()
             D_x = output.mean().item() #Mean prediction on the batch
@@ -56,7 +49,6 @@ def train(dataloader,netD,netG,optimizerD,optimizerG,num_epochs,device,savenet,p
             errD_fake = BCEsmooth(output, label,device)
             errD_fake.backward()
             D_G_z1 = output.mean().item() #Mean prediction on the batch
-
 
             ## Compute Error of the Discriminant on both batchs
             errD = errD_real + errD_fake
@@ -88,42 +80,40 @@ def train(dataloader,netD,netG,optimizerD,optimizerG,num_epochs,device,savenet,p
 
             # Output training stats
             if i % 25 == 0:
-                print('[{}/{}][{}/{}]\tLoss_D: {}\tLoss_G: {}\tD(x): {}\tD(G(z)): {} / {}'.format(
-                        epoch, 
-                        num_epochs, 
-                        i, 
-                        len(dataloader),
-                        errD.item(), 
-                        errG.item(), 
-                        D_x, 
-                        D_G_z1, 
-                        D_G_z2))
+              print('[{}/{}][{}/{}]\tLoss_D: {}\tLoss_G: {}\tD(x): {}\tD(G(z)): {} / {}'.format(
+                      epoch, 
+                      num_epochs, 
+                      i, 
+                      len(dataloader),
+                      errD.item(), 
+                      errG.item(), 
+                      D_x, 
+                      D_G_z1, 
+                      D_G_z2))
 
-            # Save Losses for plotting later
             G_losses.append(errG.item())
             D_losses.append(errD.item())
-            netD.eval()
-            netG.eval()  
-
-            
             # Check how the generator is doing by saving G's output on fixed_noise
             if (iters % 100 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
-                    clear_output(wait=True)
-                    f_noise = torch.randn(1, nz, 1, 1, device=device)
-                    fake = netG(f_noise).detach().cpu()
+                    with torch.no_grad():
+                      clear_output(wait=True)
+                      f_noise = torch.randn(1, nz, 1, 1, device=device)
+                      fake = netG(f_noise).detach().cpu()
                     faken=Normalization(fake[0])
                     display(array_to_img(np.transpose(faken,(1,2,0))))
+
             iters += 1    
         
-        if epoch%5==0 :
-            fake = netG(fixed_noise).detach().cpu()
+        if epoch%4==0 :
+            with torch.no_grad():
+              fake = netG(fixed_noise).detach().cpu()
             img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
             if (savenet):
                 torch.save(netD.state_dict(), os.path.join(pathsavenet,"netD"+str(epoch)+".pth"))
                 torch.save(netG.state_dict(), os.path.join(pathsavenet,"netG"+str(epoch)+".pth"))
             img_to_save=np.transpose(vutils.make_grid(fake, padding=2, normalize=True).cpu().numpy(),(1,2,0))
             plt.imsave(os.path.join(pathsaveimg,"grid_"+str(epoch)+".png"),img_to_save)
-            #plt.imshow(img_to_save)
+            plt.imshow(img_to_save)
             #plt.savefig("grid_"+str(epoch)+".png")
 
-    return img_list,G_losses,D_losses
+    return img_list,G_losses,D_losses,iters
